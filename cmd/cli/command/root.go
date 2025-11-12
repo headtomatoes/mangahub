cb39058f40b18ea32b72b7d93179faebbe207fa6
Anh Ne <trongle250504@gmail.com>
2025-11-12 17:10:57 +0700

    add initial command files and main entry point for mangahubCLI, placeholder file for incoming update

---
+++ cmd/cli/command/root.go
@@ -0,0 +1,44 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package command
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "mangahubCLI",
+	Short: "mangahubCLI is a CLI application for communicating",
+	Long: `mangahubCLI is a tool for user to interact with mangahub API. This application is
+built for learning purpose and personal use. User can use this application to
+search manga, get manga details, follow/unfollow manga, update reading progress,
+and more. -help or -h to see all available commands.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.mangahub.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
